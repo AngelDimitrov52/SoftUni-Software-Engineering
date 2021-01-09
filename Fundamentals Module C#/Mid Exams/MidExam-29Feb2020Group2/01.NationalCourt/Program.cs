cb6d00d65120262a672da49aa41e106946b953e6
Angel <angeldimitrov52@gmail.com>
2021-01-09 15:25:14 +0200

    Add Fundamentals Module

---
+++ Fundamentals Module C#/Mid Exams/MidExam-29Feb2020Group2/01.NationalCourt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01.NationalCourt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPer = int.Parse(Console.ReadLine());
+            int secondPer = int.Parse(Console.ReadLine());
+            int tree = int.Parse(Console.ReadLine());
+
+            int totalPer = firstPer + secondPer + tree;
+
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            int hourCount = 0;
+            int pochivka = 0;
+            while (peopleCount > 0)
+            {
+                peopleCount -= totalPer;
+                hourCount++;
+
+                if (hourCount % 3 == 0)
+                {
+                    if (peopleCount <= 0)
+                    {
+                        break;
+                    }
+                    pochivka++;
+                }
+
+
+            }
+            Console.WriteLine($"Time needed: {pochivka + hourCount}h.");
+        }
+    }
+}
---
+++ Fundamentals Module C#/Mid Exams/MidExamp - Strike/03.MovingTarget/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MovingTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targetList = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToList();
+            string comand;
+            while ((comand = Console.ReadLine()) != "End")
+            {
+                string[] comandArgs = comand.Split();
+                string com = comandArgs[0];
+                int index = int.Parse(comandArgs[1]);
+                int value = int.Parse(comandArgs[2]);
+
+                if (com == "Shoot")
+                {
+                    if (index < 0 || index >= targetList.Count)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        targetList[index] -= value;
+                        if (targetList[index] <= 0)
+                        {
+                            targetList.RemoveAt(index);
+                        }
+                    }
+                }
+                else if (com == "Add")
+                {
+                    if (index < 0 || index >= targetList.Count)
+                    {
+                        Console.WriteLine("Invalid placement!");
+                        
+
+                    }
+                    else
+                    {
+                        targetList.Insert(index, value);
+                    }
+
+                }
+                else if (com == "Strike")
+                {
+                    int start = index - value;
+                    int end = index + value;
+                    if (start < 0 || end >= targetList.Count)
+                    {
+                        Console.WriteLine("Strike missed!");
+                       
+                    }
+                    else
+                    {
+                        targetList.RemoveRange(start, value * 2 + 1);
+                    }
+
+
+
+                }
+
+
+            }
+            Console.WriteLine(string.Join("|", targetList));
+        }
+    }
+}
