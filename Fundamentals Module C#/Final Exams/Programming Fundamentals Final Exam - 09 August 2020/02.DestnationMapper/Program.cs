325b45aa8a668856c8586e9de78a92f267201be2
Angel <angeldimitrov52@gmail.com>
2021-01-09 15:50:48 +0200

    add

---
+++ Fundamentals Module C#/Final Exams/Programming Fundamentals Final Exam - 09 August 2020/02.DestnationMapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.DestnationMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(=|\/)([A-Z][A-Za-z]{2,})(\1)";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(text);
+
+            int count = 0;
+            List<string> destinations = new List<string>();
+            foreach (Match item in matches)
+            {
+                destinations.Add(item.Groups[2].ToString());
+                count += item.Groups[2].Length;
+            }
+            Console.Write("Destinations: ");
+            Console.WriteLine(string.Join(", ",destinations));
+            Console.WriteLine($"Travel Points: {count}");
+        }
+    }
+}
---
+++ Fundamentals Module C#/Homeworks and labs/01.IntroAndBasicSyntax/IntroAndBasicSyntaxExe/01.Ages/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01.Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string person = "";
+           
+
+
+            if (age >= 66)
+            {
+                person = "elder";
+            }
+            else if (age >= 20)
+            {
+                person = "adult";
+            }
+            else if (age >= 14 )
+            {
+                person = "teenager";
+            }
+            else if (age >= 3 )
+            {
+                person = "child";
+            }
+            else if (age >= 0 )
+            {
+                person = "baby";
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
