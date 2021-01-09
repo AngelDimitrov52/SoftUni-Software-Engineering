d02e51806a439431b430ed5c2b9eeec3a79be26d
Angel <angeldimitrov52@gmail.com>
2021-01-09 15:21:48 +0200

    Add Fundamentals Module

---
+++ Fundamentals Module C#/Homeworks and labs/01.IntroAndBasicSyntax/IntroAndBasicSyntaxExe/04.PrintAndSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int starNum = int.Parse(Console.ReadLine());
+            int stopNum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = starNum; i <= stopNum; i++)
+            {
+                sum += i;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
