a1260c27220b19cd1d612e1ac2d3608f2d770cf3
Angel <angeldimitrov52@gmail.com>
2021-01-18 08:08:39 +0200

    add MultidimensionalArraysLab

---
+++ 3.C# Advanced/Homeworks and labs/02.MultidimensionalArraysLab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCols = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[rowCols, rowCols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] numbers = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row,col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+
+
+
+        }
+    }
+}
