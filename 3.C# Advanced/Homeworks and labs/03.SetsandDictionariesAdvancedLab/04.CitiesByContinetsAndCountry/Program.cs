4659f0fef7853ce7d0568c0e62d43e3079a5f9e5
Angel <angeldimitrov52@gmail.com>
2021-01-22 07:55:34 +0200

    add SetsandDictionariesAdvancedLab

---
+++ 3.C# Advanced/Homeworks and labs/03.SetsandDictionariesAdvancedLab/04.CitiesByContinetsAndCountry/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents =
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] comandArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = comandArgs[0];
+                string countrie = comandArgs[1];
+                string citi = comandArgs[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+
+                }
+                if (!continents[continent].ContainsKey(countrie))
+                {
+                    continents[continent].Add(countrie, new List<string>());
+                }
+                continents[continent][countrie].Add(citi);
+            }
+
+            foreach (var continenta in continents)
+            {
+                Console.WriteLine($"{continenta.Key}:");
+
+                foreach (var countrie in continenta.Value)
+                {
+                    Console.WriteLine($"{countrie.Key} -> {string.Join(", ", countrie.Value)}");
+                }
+            }
+
+        }
+    }
+}
+
---
+++ 3.C# Advanced/Homeworks and labs/03.SetsandDictionariesAdvancedLab/06.ParkingLot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+
+            string comand;
+            while ((comand = Console.ReadLine()) != "END")
+            {
+                string[] comandArgs = comand.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string cmd = comandArgs[0];
+                string carNum = comandArgs[1];
+
+                if (cmd == "IN")
+                {
+                    cars.Add(carNum);
+                }
+                else
+                {
+                    cars.Remove(carNum);
+                }
+            }
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(Environment.NewLine , cars));
+            }
+        }
+    }
+}
