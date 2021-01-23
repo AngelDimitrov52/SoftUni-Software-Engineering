1003779612567655276e6c97f071f9f921dc8a11
Angel <angeldimitrov52@gmail.com>
2021-01-23 23:13:26 +0200

    Add SetsandDictionariesAdvancedExe

---
+++ 3.C# Advanced/Homeworks and labs/03.SetsandDictionariesAdvancedExe/02.SetsofElements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsofElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nM = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            HashSet<int> hashSetN = new HashSet<int>();
+            for (int i = 0; i < nM[0]; i++)
+            {
+                hashSetN.Add(int.Parse(Console.ReadLine()));
+            }
+
+            HashSet<int> elemints = new HashSet<int>();
+            for (int i = 0; i < nM[1]; i++)
+            {
+                    elemints.Add(int.Parse(Console.ReadLine()));  
+            }
+
+            Console.WriteLine(string.Join(" ", hashSetN.Intersect(elemints)));
+        }
+    }
+}
