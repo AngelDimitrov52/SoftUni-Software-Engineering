9ec7158969d6dc38496494d5ee7f2075ebd80b1b
Angel <angeldimitrov52@gmail.com>
2021-01-26 08:13:45 +0200

    Add Streams Files and Directories Lab

---
+++ 3.C# Advanced/Homeworks and labs/04.StreamsFilesandDirectoriesLab/01.OddLines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _01.Oddlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string curentRow = reader.ReadLine();
+                int row = 0;
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+
+                    while (curentRow != null)
+                    {
+                        if (row % 2 == 1)
+                        {
+                            writer.WriteLine(curentRow);
+                            
+                        }
+                        curentRow = reader.ReadLine();
+                        row++;
+                    }
+                }
+            }
+           
+        }
+    }
+}
