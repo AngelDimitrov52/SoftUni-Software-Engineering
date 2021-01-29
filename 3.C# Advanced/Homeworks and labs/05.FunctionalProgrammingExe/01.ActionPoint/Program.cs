83eeb82357f2322d973d14cc481ce69b87010beb
Angel <angeldimitrov52@gmail.com>
2021-01-29 12:14:15 +0200

    add Functioal Programming Exe

---
+++ 3.C# Advanced/Homeworks and labs/05.FunctionalProgrammingExe/01.ActionPoint/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> print = n => Console.WriteLine(string.Join(
+                Environment.NewLine, n));
+
+            print(names);
+
+        }
+    }
+}
