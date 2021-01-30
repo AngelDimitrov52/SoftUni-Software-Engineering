6dc9de18d1aab85eb8404d3bb3cbf24b473401aa
Angel <angeldimitrov52@gmail.com>
2021-01-30 08:09:54 +0200

    Add 0Functional Programming Exe

---
+++ 3.C# Advanced/Homeworks and labs/05.FunctionalProgrammingExe/10.PredicateParty!/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.PredicateParty_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> peoples = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string comand;
+            while ((comand = Console.ReadLine()) != "Party!")
+            {
+                string[] comandArgs = comand.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string cmd = comandArgs[0];
+                string critery = comandArgs[1];
+
+                if (critery == "StartsWith")
+                {
+                    string index = comandArgs[2];
+                    if (cmd == "Remove")
+                    {
+                        peoples = peoples.Where(n => !n.StartsWith(index)).ToList();
+                    }
+                    else if (cmd == "Double")
+                    {
+                        string[] doubles = peoples.Where(n => n.StartsWith(index)).ToArray();
+                        peoples.AddRange(doubles);
+                    }
+                }
+                else if (critery == "EndsWith")
+                {
+                    string index = comandArgs[2];
+                    if (cmd == "Remove")
+                    {
+                        peoples = peoples.Where(n => !n.EndsWith(index)).ToList();
+                    }
+                    else if (cmd == "Double")
+                    {
+                        string[] doubles = peoples.Where(n => n.EndsWith(index)).ToArray();
+                        peoples.AddRange(doubles);
+                    }
+                }
+                else if (critery == "Length")
+                {
+                    int index = int.Parse(comandArgs[2]);
+                    if (cmd == "Remove")
+                    {
+                        peoples = peoples.Where(n => n.Length == index).ToList();
+                    }
+                    else if (cmd == "Double")
+                    {
+                        string[] doubles = peoples.Where(n => n.Length == index).ToArray();
+                        peoples.AddRange(doubles);
+                    }
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", peoples.OrderBy(n=>n)));
+        }
+    }
+}
