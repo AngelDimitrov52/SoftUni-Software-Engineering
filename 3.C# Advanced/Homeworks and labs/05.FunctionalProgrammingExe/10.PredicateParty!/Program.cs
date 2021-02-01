856ffe5a8dc13934de63ad13354f4b2e7256ef00
Angel <angeldimitrov52@gmail.com>
2021-02-01 08:09:15 +0200

    Add Functional Programming Exe

--- 3.C# Advanced/Homeworks and labs/05.FunctionalProgrammingExe/10.PredicateParty!/Program.cs
+++ 3.C# Advanced/Homeworks and labs/05.FunctionalProgrammingExe/10.PredicateParty!/Program.cs
@@ -11,56 +11,73 @@ namespace _10.PredicateParty_
             List<string> peoples = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
+            Func<List<string>, HashSet<string>, List<string>> doublePeople = (a, b) =>
+            {
+                foreach (string doubled in b)
+                {
+                    for (int i = 0; i < a.Count * 2; i++)
+                    {
+                        if (i < a.Count)
+                        {
+                            if (a[i] == doubled)
+                            {
+                                a.Insert(i, doubled);
+                                i++;
+                            }
+                        }
+                    }
+                    
+                }
 
+                return a;
+            };
+            HashSet<string> filterPeople = new HashSet<string>();
             string comand;
             while ((comand = Console.ReadLine()) != "Party!")
             {
                 string[] comandArgs = comand.Split(" ", StringSplitOptions.RemoveEmptyEntries);
                 string cmd = comandArgs[0];
                 string critery = comandArgs[1];
+                string index = comandArgs[2];
 
                 if (critery == "StartsWith")
-                {
-                    string index = comandArgs[2];
-                    if (cmd == "Remove")
-                    {
-                        peoples = peoples.Where(n => !n.StartsWith(index)).ToList();
-                    }
-                    else if (cmd == "Double")
-                    {
-                        string[] doubles = peoples.Where(n => n.StartsWith(index)).ToArray();
-                        peoples.AddRange(doubles);
-                    }
-                }
-                else if (critery == "EndsWith")
-                {
-                    string index = comandArgs[2];
-                    if (cmd == "Remove")
-                    {
-                        peoples = peoples.Where(n => !n.EndsWith(index)).ToList();
-                    }
-                    else if (cmd == "Double")
-                    {
-                        string[] doubles = peoples.Where(n => n.EndsWith(index)).ToArray();
-                        peoples.AddRange(doubles);
-                    }
-                }
-                else if (critery == "Length")
-                {
-                    int index = int.Parse(comandArgs[2]);
-                    if (cmd == "Remove")
-                    {
-                        peoples = peoples.Where(n => n.Length == index).ToList();
-                    }
-                    else if (cmd == "Double")
-                    {
-                        string[] doubles = peoples.Where(n => n.Length == index).ToArray();
-                        peoples.AddRange(doubles);
-                    }
+                {             
+                    filterPeople = peoples.Where(n => n.StartsWith(index)).ToHashSet();
                 }
 
+                else if (critery == "EndsWith")
+                {   
+                    filterPeople = peoples.Where(n =>n.EndsWith(index)).ToHashSet();
+                }
+
+                else if (critery == "Length")
+                {
+                    int indexx = int.Parse(index);
+                    filterPeople = peoples.Where(n => n.Length == indexx).ToHashSet();
+                }
+
+                if (cmd == "Remove")
+                {
+                    peoples = peoples.Where(n => !filterPeople.Contains(n)).ToList();
+                }
+
+                else if (cmd == "Double")
+                {
+                    peoples = doublePeople(peoples, filterPeople);
+                }
             }
-            Console.WriteLine(string.Join(", ", peoples.OrderBy(n=>n)));
+
+            if (peoples.Count > 0)
+            {
+                Console.WriteLine($"{string.Join(", ", peoples)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine($"Nobody is going to the party!");
+            }
+
+
+
         }
     }
 }
