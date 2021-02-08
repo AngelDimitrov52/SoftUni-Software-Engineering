98493d5b7206fd47516b18b20d0e3339c97fc01b
Angel <angeldimitrov52@gmail.com>
2021-02-08 23:17:40 +0200

    Add Generics

---
+++ 3.C# Advanced/Homeworks and labs/09.Gemerics/ArrayCreator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArr = ArrayCreator.Create(5, "Angel");
+            int[] intArr = ArrayCreator.Create(6, 17);
+
+        }
+    }
+}
