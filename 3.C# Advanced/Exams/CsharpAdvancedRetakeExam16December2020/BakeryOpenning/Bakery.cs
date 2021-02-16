a2c395105c53aa622b4e53628d9ade58efeca33e
Angel <angeldimitrov52@gmail.com>
2021-02-16 21:20:51 +0200

    Add Exam

---
+++ 3.C# Advanced/Exams/CsharpAdvancedRetakeExam16December2020/BakeryOpenning/Bakery.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            Capacity = capacity;
+            Name = name;
+            data = new List<Employee>(capacity);
+
+        }
+        public string Name { get; }
+        public int Capacity { get; }
+        public int Count { get => data.Count; }
+
+        public void Add(Employee employee)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+        public bool Remove(string name)
+        {
+            foreach (var item in data)
+            {
+                if (item.Name == name)
+                {
+                    data.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(n => n.Age).FirstOrDefault();
+        }
+        public Employee GetEmployee(string name)
+        {
+            foreach (var item in data)
+            {
+                if (item.Name == name)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {this.Name}:");
+            foreach (var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 3.C# Advanced/Exams/CsharpAdvancedRetakeExam16December2020/P01.Cooking/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> liquids = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Stack<int> ingredients = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            int breadCount = 0;
+            int cakeCount = 0;
+            int pastryCount = 0;
+            int fluidCount = 0;
+
+            while (liquids.Count > 0 && ingredients.Count > 0)
+            {
+                int firstLiquid = liquids.Dequeue();
+                int firstIngredient = ingredients.Pop();
+                int sum = firstLiquid + firstIngredient;
+
+                if (sum == 25)
+                {
+                    breadCount++;
+                }
+                else if (sum == 50)
+                {
+                    cakeCount++;
+                }
+                else if (sum == 75)
+                {
+                    pastryCount++;
+                }
+                else if (sum == 100)
+                {
+                    fluidCount++;
+                }
+                else
+                {
+                    ingredients.Push(firstIngredient + 3);
+                }
+            }
+
+            if (breadCount > 0 && cakeCount > 0 && pastryCount > 0 && fluidCount > 0)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (liquids.Count == 0)
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+
+
+            if (ingredients.Count == 0)
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", ingredients)}");
+            }
+
+            Console.WriteLine($"Bread: {breadCount}");
+            Console.WriteLine($"Cake: {cakeCount}");
+            Console.WriteLine($"Fruit Pie: {fluidCount}");
+            Console.WriteLine($"Pastry: {pastryCount}");
+        }
+    }
+}
