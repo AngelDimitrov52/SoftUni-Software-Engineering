1c8d767889a89cff9e3d59cc79152bc27fba02a7
Angel <angeldimitrov52@gmail.com>
2021-02-25 23:10:26 +0200

    Add Inheritance Exe

---
+++ 4.C# OOP/Homeworks and labs/01.InheritanceExe/Zoo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Bear bear = new Bear("Bear");
+            string ad = bear.Name;
+            Console.WriteLine(ad);
+        }
+    }
+}
