fd31e03db52745b675cba9f8d91cf00de5d7425c
Angel <angeldimitrov52@gmail.com>
2021-03-02 23:19:19 +0200

    Add Encapsulation Exe

---
+++ 4.C# OOP/Homeworks and labs/02.EncapsulationExe/P03.Topping/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.ShoppingSpree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Person> persons = new Dictionary<string, Person>();
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            try
+            {
+                string[] firstPersons = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var person in firstPersons)
+                {
+                    string[] elements = person.Split("=", StringSplitOptions.RemoveEmptyEntries);
+                    string name = elements[0];
+                    int money = int.Parse(elements[1]);
+                    Person per = new Person(name, money);
+                    persons.Add(name, per);
+                }
+
+
+                string[] secondProducts = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string produst in secondProducts)
+                {
+                    string[] elements = produst.Split("=", StringSplitOptions.RemoveEmptyEntries);
+                    string name = elements[0];
+                    int cost = int.Parse(elements[1]);
+                    Product per = new Product(name, cost);
+                    products.Add(name, per);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            string comand;
+            while ((comand = Console.ReadLine()) != "END")
+            {
+                string[] comandArgs = comand.Split();
+                string personName = comandArgs[0];
+                string productName = comandArgs[1];
+
+                Person thisPerson = persons[personName];
+                Product thisProduct = products[productName];
+
+                thisPerson.AddProduct(thisProduct);
+
+            }
+
+            foreach (var person in persons)
+            {
+                if (person.Value.Bag.Count == 0)
+                {
+                    Console.WriteLine($"{person.Value.Name} - Nothing bought");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Value.Name} - {string.Join(", ", person.Value.Bag.Select(p => p.Name))}");
+                }
+            }
+        }
+    }
+}
---
+++ 4.C# OOP/Homeworks and labs/02.EncapsulationExe/P04.PizzaCalories/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P04.PizzaCalories
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string[] pizzaname = Console.ReadLine().Split();
+                Pizza pizza = new Pizza(pizzaname[1]);
+
+                string[] doughName = Console.ReadLine().Split();
+                pizza.Dougt = new Dough(doughName[1], doughName[2], int.Parse(doughName[3]));
+
+                string comand;
+                while ((comand = Console.ReadLine()) != "END")
+                {
+                    string[] toppingName = comand.Split();
+                    Topping top = new Topping(toppingName[1], int.Parse(toppingName[2]));
+                    pizza.AddTopping(top);
+
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.Calories:F2} Calories.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
