0ecc0bbc5902dcd72b8ba8e11f56a1fe5d1a0df0
Angel <angeldimitrov52@gmail.com>
2021-03-04 22:18:41 +0200

    Add Interfaces And Abstraction Exe

---
+++ 4.C# OOP/Homeworks and labs/03.InterfacesAndAbstractionExe/P03.Telephony/Smartphone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.Telephony
+{
+    public class Smartphone : ICall, IBrowsing
+    {
+
+        public string Browsing(string web)
+        {
+            foreach (var element in web)
+            {
+                if (char.IsDigit(element))
+                {
+                    return "Invalid URL!";
+                }
+            }
+            return $"Browsing: {web}!";
+
+        }
+
+        public string Call(string numbers)
+        {
+            foreach (var element in numbers)
+            {
+                if (!char.IsDigit(element))
+                {
+                    return $"Invalid number!";
+                }
+            }
+
+            return $"Calling... {numbers}";
+
+        }
+    }
+}
+
+
+
+
+
---
+++ 4.C# OOP/Homeworks and labs/03.InterfacesAndAbstractionExe/P04.BorderControl/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IIdentifiable> Ids = new List<IIdentifiable>();
+            string comand;
+            while ((comand = Console.ReadLine()) != "End")
+            {
+                string[] comandArgs = comand.Split();
+                if (comandArgs.Length == 3)
+                {
+                    Ids.Add(new Citizen(comandArgs[0], int.Parse(comandArgs[1]), comandArgs[2]));
+                }
+                else
+                {
+                    Ids.Add(new Robot(comandArgs[0], comandArgs[1]));
+                }
+            }
+
+            comand = Console.ReadLine();
+
+            List<IIdentifiable> filter = Ids.Where(p => p.Id.EndsWith(comand)).ToList();
+            foreach (var item in filter)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+        }
+    }
+}
