3c6076c9485f4f6d8333f27fe76fa399d3a72126
Angel <angeldimitrov52@gmail.com>
2021-03-07 23:59:58 +0200

    Add Interfaces And Abstractions Exe

---
+++ 4.C# OOP/Homeworks and labs/03.InterfacesAndAbstractionExe/P07.MilitaryElite/Abstractclasses/Repair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07.MilitaryElite.Abstractclasses
+{
+    public class Repair
+    {
+        public Repair( string name , int hours)
+        {
+            Name = name;
+            Hours = hours;
+
+        }
+        public string Name { get; private set; }
+        public int Hours { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Part Name: {Name} Hours Worked: {Hours}";     
+        }
+
+    }
+}
---
+++ 4.C# OOP/Homeworks and labs/03.InterfacesAndAbstractionExe/P07.MilitaryElite/Abstractclasses/Soldier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07.MilitaryElite.Abstractclasses
+{
+    public abstract class Soldier
+    {
+        public Soldier(string id, string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Id { get; private set; }
+    }
+}
