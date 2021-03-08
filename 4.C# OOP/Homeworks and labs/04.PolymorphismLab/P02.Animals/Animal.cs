c42e72ec3f08a74ecf3541ac6278598e0d4e11ca
Angel <angeldimitrov52@gmail.com>
2021-03-08 08:07:01 +0200

    Added Polymorphism Lab

---
+++ 4.C# OOP/Homeworks and labs/04.PolymorphismLab/P02.Animals/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string food)
+        {
+            Name = name;
+            FavouriteFood = food;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                name = value;
+            }
+        }
+        public string FavouriteFood
+        {
+            get => this.favouriteFood;
+            set
+            {
+                favouriteFood = value;
+            }
+        }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood}";
+        }
+
+    }
+}
