e72152bdc5e57bbb90ada89dbf72f4118790e208
Angel <angeldimitrov52@gmail.com>
2021-03-11 20:38:39 +0200

    Added Exception handling lecture

---
+++ 4.C# OOP/Homeworks and labs/05.ExceptionHandling/P02.EnterNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 0;
+            int end = 100;
+
+            try
+            {
+                ReadNumber(start, end);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Try again");
+                ReadNumber(start, end);
+            }
+        }
+
+        private static void ReadNumber(int start, int end)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(int.Parse(Console.ReadLine()));
+
+            for (int i = 1; i < 10; i++)
+            {
+                int digit = int.Parse(Console.ReadLine());
+                if (digit <= numbers[i - 1])
+                {
+                    throw new ArgumentException();
+                }
+
+                numbers.Add(digit);
+            }
+        }
+    }
+}
