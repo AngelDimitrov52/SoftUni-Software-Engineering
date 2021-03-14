0797306b5c1d5708cfc1607190dcfee635caf201
Angel <angeldimitrov52@gmail.com>
2021-03-14 14:32:28 +0200

    Add SOLID Lab

---
+++ 4.C# OOP/Homeworks and labs/06.SOLID.Lab/P03.Detail_Printer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Angel");
+            Manager manager = new Manager("Dany", new List<string>() { "File", "Data", "Web" });
+
+
+
+            DetailsPrinter detailsPrinter = new DetailsPrinter(new List<IPersons>() { employee, manager });
+            detailsPrinter.PrintDetails();
+        }
+    }
+}
---
+++ 4.C# OOP/Homeworks and labs/06.SOLID.Lab/P04.Recharge/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P04.Recharge
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Employee employee = new Employee("32");
+            Robot robot = new Robot("34", 5);
+
+            employee.Sleep();
+            robot.Work(55);
+
+        }
+    }
+}
