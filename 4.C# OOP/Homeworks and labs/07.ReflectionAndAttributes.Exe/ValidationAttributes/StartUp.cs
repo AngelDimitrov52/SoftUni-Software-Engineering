ccafc263e095c11f6460ed69ec71ba8427e14619
Angel <angeldimitrov52@gmail.com>
2021-03-21 18:08:00 +0200

    Add Reflection And Attributes Exe

---
+++ 4.C# OOP/Homeworks and labs/07.ReflectionAndAttributes.Exe/ValidationAttributes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person("Angel", 14);
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
