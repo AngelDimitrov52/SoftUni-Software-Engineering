7e92898d7032f3b4863f35d81696d9437a0dae6e
Angel <angeldimitrov52@gmail.com>
2021-04-04 23:18:27 +0300

    Added Exam

---
+++ 4.C# OOP/Exams/C# OOP Retake Exam - 19 December 2020/01. Structure_Skeleton/Entities/Characters/Character.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+using WarCroft.Constants;
+using WarCroft.Entities.Inventory.Contracts;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Characters.Contracts
+{
+    public abstract class Character
+    {
+        // TODO: Implement the rest of the class.
+        private string name;
+        private double baseHealth;
+        private double baseArmor;
+        private double health;
+        private double armor;
+
+        public Character(string name, double health, double armor, double abilityPoints, Bag bag)
+        {
+            Name = name;
+            baseHealth = health;
+            Health = health;
+            baseArmor = armor;
+            Armor = armor;
+            AbilityPoints = abilityPoints;
+            Bag = bag;
+            IsAlive = true;
+        }
+
+        public string Name
+        {
+            get => name;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.CharacterNameInvalid);
+                }
+                name = value;
+            }
+        }
+        public double BaseHealth
+        {
+            get => baseHealth;
+
+            private set
+            {
+                baseHealth = value;
+            }
+        }
+        public double Health
+        {
+            get => health;
+            set
+            {
+                if (value >= BaseHealth)
+                {
+                    health = BaseHealth;
+                }
+                health = value;
+                if (health < 0)
+                {
+                    health = 0;
+                }
+            }
+        }
+
+        public double BaseArmor
+        {
+            get => baseArmor;
+
+            private set
+            {
+                baseArmor = value;
+            }
+        }
+        public double Armor
+        {
+            get => armor;
+            set
+            {
+                if (value >= BaseArmor)
+                {
+                    armor = BaseArmor;
+                }
+                armor = value;
+            }
+        }
+        public double AbilityPoints { get; private set; }
+
+        public Bag Bag { get; private set; }
+
+        public bool IsAlive { get; set; } = true;
+
+        public void TakeDamage(double hitPoints)
+        {
+            this.EnsureAlive();
+
+            if (Armor == 0)
+            {
+                Health -= hitPoints;
+            }
+
+            Armor -= hitPoints;
+            if (Armor < 0)
+            {
+                double most = Math.Abs(Armor);
+                Armor = 0;
+
+            }
+
+            if (Health <= 0)
+            {
+                IsAlive = false;
+            }
+
+        }
+        public void UseItem(Item item)
+        {
+            item.AffectCharacter(this);
+        }
+        protected void EnsureAlive()
+        {
+            if (!this.IsAlive)
+            {
+                throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+            }
+        }
+    }
+}
---
+++ 4.C# OOP/Exams/C# OOP Retake Exam - 19 December 2020/01. Structure_Skeleton/Entities/Inventory/Bag.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WarCroft.Constants;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Inventory.Contracts
+{
+    public abstract class Bag : IBag
+    {
+        private int capacity;
+        private List<Item> items;
+        public Bag(int capacity)
+        {
+            Capacity = capacity;
+            items = new List<Item>();
+        }
+        public int Capacity { get; set; } = 100;
+
+
+        public int Load => items.Sum(p => p.Weight);
+
+        public IReadOnlyCollection<Item> Items => items;
+
+        public void AddItem(Item item)
+        {
+            if (Load + item.Weight > capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.ExceedMaximumBagCapacity);
+            }
+            items.Add(item);
+        }
+
+        public Item GetItem(string name)
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException(ExceptionMessages.EmptyBag);
+            }
+            var item = items.FirstOrDefault(p => p.GetType().Name == name);
+
+            if (item == null)
+            {
+                throw new ArgumentException($"No item with name {name} in bag!");
+            }
+
+            items.Remove(item);
+            return item;
+
+        }
+    }
+}
