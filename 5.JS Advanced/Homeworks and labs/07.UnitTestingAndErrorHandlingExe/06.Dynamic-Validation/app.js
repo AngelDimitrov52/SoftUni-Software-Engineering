38c0bca891cd5e26dd5c4eac14940fa5054670cd
Angel <angeldimitrov52@gmail.com>
2021-06-17 17:56:45 +0300

    Added Unit testing and error handing

--- 5.JS Advanced/Homeworks and labs/07.UnitTestingAndErrorHandlingExe/06.Dynamic-Validation/app.js
+++ 5.JS Advanced/Homeworks and labs/07.UnitTestingAndErrorHandlingExe/06.Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    // TODO
+    let emailElemet = document.getElementById('email');
+    emailElemet.addEventListener('change', validate)
+
+    function validate() {
+        let reg = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        let ismach = reg.test(emailElemet.value);
+
+        if (ismach) {
+            emailElemet.className = '';
+        } else {
+            emailElemet.className = 'error';
+        }
+
+    }
 }
