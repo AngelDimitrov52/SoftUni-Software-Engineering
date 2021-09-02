8c27469347d435572377e1a5f819e7d98225291d
Angel <angeldimitrov52@gmail.com>
2021-09-02 23:07:50 +0300

    Added exam

---
+++ 4.C# OOP/Exams/01. Structure_Skeleton/AquaShop/Models/Decorations/Plant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Decorations
+{
+    class Plant : Decoration
+    {
+        private const decimal price = 10;
+        private const int comfort = 5;
+
+        public Plant()
+            : base(comfort, price)
+        {
+
+        }
+    }
+}
